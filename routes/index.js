95833bf804650ff0f1adb1913f880cfdab2e6ff1
pdineshyadav <p.dineshyadav@yahoo.in>
2019-02-03 20:53:21 -0500

    displayed db in view

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,6 @@
 let express = require('express');
 let router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
